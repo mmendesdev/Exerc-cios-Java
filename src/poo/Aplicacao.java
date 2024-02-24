423968b8b5677b45128223db5733c4abde588110
Matheus Mendes <matheus.mendes@somosicev.com>
2024-02-24 01:02:25 -0300

    commit POO

---
+++ src/poo/Aplicacao.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa();
+        p1.nome = "Matheus";
+
+        Pessoa p2 = new Pessoa();
+        p2.nome = "Maria";
+
+        p1.receber(10);
+        p1.dar(5, p2);
+
+        System.out.println(p1.nome + " = Figurinhas:" + p1.numFigurinhas);
+        System.out.println(p2.nome + " = Figurinhas:" + p2.numFigurinhas);
+    }
+    
+}
--- src/poo/Pessoa.java
+++ src/poo/Pessoa.java
@@ -1,5 +1,18 @@
 package poo;
 
 public class Pessoa {
+    //atributos
+   String nome;
+   int numFigurinhas;
+
+   //metodos = funcao
+    void receber(int numFigurinhas){
+        this.numFigurinhas += numFigurinhas;
+    }
+    void dar(int numFigurinhas, Pessoa p){
+          this.numFigurinhas -= numFigurinhas;
+          p.numFigurinhas += numFigurinhas;
+    }
+
     
 }
